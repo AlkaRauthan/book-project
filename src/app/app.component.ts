464103a34522be8966318ef4cb3f3fde6d546dea
Alka <alkarauthan@optum.com>
2019-09-15 15:49:47 +0530

    fetched facebook graph api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graph-api';
+  posts = [];
+  pageDetails;
+  constructor(private http : HttpClient){
+
+  }
+
+  ngOnInit(){
+    const token = 'EAALvJXQfjxQBAMDZAuYFoyc3AYqy99TieZBbj7d2Mf3oEBOza7vsoWGLkheTbCTqEZCucHmtm7A0QnZBFRjVsbjY5JIq2lU5tPMUtMXydKkKb3Gx0tYkkBpQIPwYOl8nweP4a8BYtmo1IViFfi9n9xGIkGUjJZBivNIrEjAib7wZDZD';
+    let url = 'https://graph.facebook.com/750632711777207?fields=location,name,is_permanently_closed,page_token,posts';
+    const tokenString = `&access_token=${token}`;
+    url = url + tokenString;
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept', 'application/json');
+    headers = headers.append('Authorization', `${token}`);
+    return this.http
+    .get(url)
+    .subscribe((response:any) => {
+      this.pageDetails = response;
+      this.posts = response.posts.data;
+    })
+
+  }
+
+    
 }
